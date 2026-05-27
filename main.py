1958cf88fa48a3570ff88c4348dc9bd7cf11a74f
Rory Healy <166872300+sunken-bishop@users.noreply.github.com>
2026-05-27 15:01:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pathlib, shutil, argparse
+
+parser = argparse.ArgumentParser(description="Process some files.")
+parser.add_argument("src")
+parser.add_argument("dst")
+
+args = parser.parse_args()
+
+shutil.move(args.src, args.dst)
+
